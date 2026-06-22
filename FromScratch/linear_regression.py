9011641c218c00f89011ffd87e0352f5faaa357f
Wayn-Git <bilalrukundi1658@gmail.com>
2026-06-23 01:34:29 +0530

    Implimented linear regression completely from scratch

--- FromScratch/linear_regression.py
+++ FromScratch/linear_regression.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 """ Formula for linear regression: y = mx + b
 Where m = weight and b = bais 
@@ -9,25 +11,57 @@ formula for m = sum(x - mean(x) * (y - mean(y)))
 
 """
 
-# The number of training data
-N = 200
-# 200 random samples as our data
-x_1 = np.random.rand(N)
-# Define the line slope and the Gaussian noise parameters
-slope = 3
-mu, sigma = 0, 0.1 # mean and standard deviation
-intercept = np.random.normal(mu, sigma, N)
-# Define the coordinates of the data points using the line equation and the added Gaussian noise 
-y = slope*x_1 + intercept
+X = np.array([1, 2, 3, 4, 5], dtype=float)
+
+y = np.array([2, 4, 6, 8, 10], dtype=float)
+
 
 class LinearRegression:
-    def __init__(self, x, y, lr=0.1, w=0, b=0):
-        self.x =  x
-        self.y = y
+    def __init__(self, lr_rate, epochs):
+        self.lr_rate = lr_rate
+        self.epochs = epochs
+        self.w = None
+        self.b = None
+    #https://machinelearningmastery.com/linear-regression-for-machine-learning/
+    def predict(self, X):
+        return X * self.w + self. b
     
-    def MSE(self, y_pred):
-        return np.mean((self.y - y_pred) ** 2)
+    # https://www.geeksforgeeks.org/python/python-mean-squared-error/
+    def MSE(self, y_pred, y_true):
+
+        return np.mean(y_true - y_pred)**2
+
+    # yi = y_true, m = w, b = b, xi = X
+    # https://www.geeksforgeeks.org/machine-learning/gradient-descent-in-linear-regression/
+    def gradient(self, X, y, loss):
+
+        wg = -2 * (np.mean(X *(y - (self.w * X + self.b)))) 
+        wb = -2 * (np.mean((y - (self.w * X + self.b)))) 
+
+        return wg, wb
     
-    
-x = np.random.randint(10)
-print(x)
+    def fit(self, X, y):
+
+        self.w = np.random.randn(1, 1)
+        self.b = np.random.randn(1)
+        for epoch in range(self.epochs):
+            y_pred = self.predict(X)
+
+            loss = self.MSE(y_pred, y)
+
+            wg, wb = self.gradient(X, y, loss)
+
+            self.w -= self.lr_rate * wg
+            self.b -= self.lr_rate * wb
+            
+            if epoch % 100 == 0:
+                print(f"epoch: {epoch}, Loss: {loss:.6f}")
+
+
+
+
+       
+lr_model = LinearRegression(0.01, 1000)
+
+lr_model.fit(X, y)
+
